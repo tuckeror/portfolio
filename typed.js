50151102a1b9096a5c4f61178c2b8d4275540df8
tuckeror <70530469+tuckeror@users.noreply.github.com>
2020-12-16 22:41:05 -0500

    Add files via upload

--- typed.js
+++ typed.js
@@ -1,21 +1,37 @@
-const text = ['designer', 'coder', 'artist'];
+const texts = ['designer', 'coder', 'artist'];
 let count = 0;
 let index = 0;
 let currentText = "";
 let letter = "";
 
 (function type() {
-    if (count === texts.length){
+    if(count === texts.length){
         count = 0;
     }
     currentText = texts[count];
     letter = currentText.slice(0, ++index);
     
-    document.querySelector("typing").textContent = letter;
+    document.querySelector(".typing").textContent = letter;
     if(letter.length === currentText.length){
         count++;
         index = 0;
     }
-    setTimeout(type, 400);
+    setTimeout(type, 250);
 })();
 
+
+$(document).ready(function(){
+  $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+    
+    $(".zoom").hover(function(){
+		
+		$(this).addClass('transition');
+	}, function(){
+        
+		$(this).removeClass('transition');
+	});
+});
+    
